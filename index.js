9389f6184d057e5eb9c6ac280e08976479d49a03
Tuguldur Tuvshintugs <146443087+tuguldur0@users.noreply.github.com>
2026-07-22 10:51:23 +0800

    test

---
+++ index.js
@@ -0,0 +1,66 @@
+const numbers = ["del", "clear", "%", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, "."];
+const numbs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const calculator = document.getElementById("calculator");
+const equals = document.createElement("button")
+const newArr = numbers.map(displayNums);
+const input = document.getElementById("input");
+
+equals.textContent = "="
+equals.classList.add("button")
+calculator.appendChild(equals);
+
+function displayNums(num) {
+    let button = document.createElement("button");
+    button.textContent = num;
+    button.classList.add("button")
+    calculator.appendChild(button);
+    
+    button.addEventListener("click", function(){
+        for(let i = 0; i < 10; i++){
+            if(num == i){
+                input.value += num;
+            }
+        }
+        if(num == "del"){
+            input.value = input.value.substring(0, [input.value.length-1]);
+        } else if (num == "clear"){
+            input.value = "";
+        } else if (num == "%"){
+            let temp0 = input.value;
+            input.value = ""
+            equals.addEventListener("click", function(){
+                     input.value = temp0 % input.value;
+            })
+        } else if (num == "/"){
+            let temp1 = input.value;
+            input.value = ""
+            equals.addEventListener("click", function(){
+                input.value = temp1 / input.value;
+            })
+                        
+        } else if (num == "x"){
+            let temp2 = input.value;
+            input.value = ""
+            equals.addEventListener("click", function(){
+                input.value = temp2 * input.value;
+            })
+                        
+        } else if (num == "-"){
+            let temp3 = input.value;
+            input.value = ""
+            equals.addEventListener("click", function(){
+                input.value = temp3 - input.value;
+            })
+        } else if (num == "+"){
+            let temp3 = input.value;
+            input.value = ""
+            equals.addEventListener("click", function(){
+                input.value = temp3 + input.value;
+            })
+        } else if (num == "."){
+            input.value += "."
+        } else if (num == "+/-"){
+            input.value *= -1;
+        }
+    })
+}
