69266744e2511ac0f618a294a47b58f1eeae3a83
Tuguldur Tuvshintugs <146443087+tuguldur0@users.noreply.github.com>
2026-07-24 09:41:47 +0800

    jjj

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const numbers = ["del", "clear", "%", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, "."];
+const numbers = ["del", "AC", "%", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, "."];
 const calculator = document.getElementById("calculator");
 const equals = document.createElement("button")
 const operators = ["%", "/", "x", "-", "+"];
@@ -6,10 +6,24 @@ const newArr = numbers.map(displayNums);
 const input = document.getElementById("input");
 let currentOperator;
 let temp1;
+let temp2;
 equals.textContent = "="
 equals.classList.add("button")
 calculator.appendChild(equals);
 
+function operate(num1, num2, op){
+    if(op == "%"){
+        return num1 % num2;
+    } else if(op == "/"){
+        return num1 / num2;
+    } else if(op == "x"){
+        return num1 * num2;
+    } else if(op == "-"){
+        return num1 - num2;
+    } else if(op == "+"){
+        return num1 + num2;
+    }
+}
 function displayNums(num) {
     let button = document.createElement("button");
     button.textContent = num;
@@ -32,28 +46,22 @@ function displayNums(num) {
             input.value = input.value.substring(0, input.value.length-2);
         } else if (num == 0 && input.value != ""){
             input.value += num;
-        } else if (num == "clear"){
+        } else if (num == "AC"){
             input.value = "";
-        } else if (num == "%"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "/"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "x"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "-"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "+"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
+            temp1 = undefined;
+            temp2 = undefined;
+            currentOperator = undefined;
+        } else if (operators.includes(num)){
+            if(temp1 == undefined && currentOperator == undefined){
+                currentOperator = num;
+                temp1 = Number(input.value);
+                input.value = "";
+            } else {
+                temp2 = Number(input.value);
+                temp1 = operate(temp1, temp2, currentOperator);
+                currentOperator = num;
+                input.value = "";
+            }
         } else if (num == "+/-"){
             if(input.value == ""){
                 input.value = -1;
@@ -61,40 +69,17 @@ function displayNums(num) {
                 input.value = Number(input.value) * -1
             }
         } else if(num == "."){
+            if(!input.value.includes(".")){
             input.value = String(input.value) + "."
+            }
         } 
         
     })
 
 }
-const operatorsButton = document.querySelector(".operators")
-operatorsButton.addEventListener("click", function(){
-    if (num == "%"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "/"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "x"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "-"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        } else if (num == "+"){
-            currentOperator = num;
-            temp1 = Number(input.value);
-            input.value = "";
-        }
-})
-
 
 equals.addEventListener("click", function(){
-    let temp2 = Number(input.value);
+    temp2 = Number(input.value);
     if(input.value != ""){
     if(currentOperator == "%"){
         result = temp1 % temp2;
